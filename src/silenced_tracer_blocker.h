18ae074ea6d872cc1c24b87e877cddbd6e3d4f22
✪ Stαr <oifancy1@gmail.com>
2026-06-03 06:12:51 +0300

    .

--- src/silenced_tracer_blocker.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// CS2Fixes-style patch module.
-// Add InstallSilencedTracerBlocker() on plugin load.
-// Add RemoveSilencedTracerBlocker() on plugin unload.
-
-void InstallSilencedTracerBlocker();
-void RemoveSilencedTracerBlocker();
